b027ce09140b521954a4cc535f4d3bcebcfec980
Wangxinyu <2397303486@qq.com>
2024-03-04 11:18:37 +0800

    IDEA first commit

---
+++ src/main/java/com/yu/service/GitTest.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Package: com.yu.service
+ * Type:    GitTest
+ * Date:    2024/3/4 11:09
+ *
+ * Copyright (c) 2024 HUANENG GUICHENG TRUST CORP.,LTD All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the License.
+ *******************************************************************************/
+package com.yu.service;
+
+/**
+ *
+ * @author Wangxinyu
+ * @date 2024/3/4 11:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
